68318deeb58b831f72ff57ca7afa51fbbc8bad7b
123Riho <ronja.olvall@iths.se>
2025-10-13 13:21:31 +0200

    Adding category to the home page and creating a separate categorypage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import RecipeList from './components/RecipeList'
 import Homepage from './components/Homepage'
+import Categorypage from './components/Categorypage'
 import './App.css'
 
 function App() {
   return (
     <Homepage/>
+    //<Categorypage/>
   )
 }
 
