fa160c862d2997392eb0c53f56083bda266a00e0
Ronja Olvall <ronja.olvall@iths.se>
2025-10-23 10:42:25 +0200

    Update RecipeList.jsx

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -33,8 +33,9 @@ function RecipeList() {
       <div className="recipe-grid">
         {filteredRecipes.length > 0 ? (
           filteredRecipes.map((recipe) => (
-            <Link to={`/recipe/${recipe._id}`} />
+            <Link to={`/recipe/${recipe._id}`} >
             <RecipeCard key={recipe._id} recipe={recipe} />
+            </Link>  
           ))
         ) : searchQuery ? (
           <p>Inga recept hittades för "{searchQuery}"</p>
